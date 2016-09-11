fdbdb0fd8672b40f0425f1d9d8e6c285940f626b
Mayank Ranjan Dayal <mayankrd@ccs.neu.edu>
2016-09-11 19:52:06 -0400

    android mobile app development project complete

---
+++ app/src/main/java/edu/neu/madcourse/mayankranjandayal/Acknowledgement.java
@@ -0,0 +1,17 @@
+package edu.neu.madcourse.mayankranjandayal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Acknowledgement extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acknowledgement);
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+        getIntent();
+    }
+
+}
